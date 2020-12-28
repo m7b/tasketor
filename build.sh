c5b8ed7dcbb9ba657a7baff7a8d41aaa97781bee
Mark Buttig <mbuttig@gmx.net>
2020-12-28 16:02:16 +0100

    Build script for linux

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#install libs (boost, mariadb) and cmake
+sudo apt-get -yq --no-install-suggests --no-install-recommends install libboost-all-dev libmariadb-dev cmake
+
+rm -r build
+mkdir build
+cd build
+cmake ..
+make
+cd ..
