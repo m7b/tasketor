6156f79935e566696e630949449dc5ee28898fcd
Mark Buttig <mbuttig@gmx.net>
2021-02-21 14:41:22 +0100

    change travis

--- scripts/ci/update_cmake.sh
+++ scripts/ci/update_cmake.sh
@@ -27,16 +27,14 @@ travis_retry() {
 DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
 mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
 # we use wget to fetch the cmake binaries
-#travis_retry wget --no-check-certificate https://cmake.org/files/v3.20/cmake-3.20.0-rc1-linux-x86_64.tar.gz
-wget --no-check-certificate https://cmake.org/files/v3.20/cmake-3.20.0-rc1-linux-x86_64.tar.gz
+travis_retry wget --no-check-certificate https://cmake.org/files/v3.20/cmake-3.20.0-rc1-linux-x86_64.tar.gz
 # this is optional, but useful:
 # do a quick md5 check to ensure that the archive we downloaded did not get compromised
 echo "455ff919e07f3afb938abd3c0ee7f234b690699f4980065f6001407995b0e6b4 *cmake-3.20.0-rc1-linux-x86_64.tar.gz" > cmake_sha256.txt
 sha256sum -c cmake_sha256.txt
 # extract the binaries; the output here is quite lengthy,
 # so we swallow it to not clutter up the travis console
-#tar -xvf cmake-3.20.0-rc1-linux-x86_64.tar.gz > /dev/null
-tar -xvf cmake-3.20.0-rc1-linux-x86_64.tar.gz
+tar -xvf cmake-3.20.0-rc1-linux-x86_64.tar.gz > /dev/null
 mv cmake-3.20.0-rc1-linux-x86_64 cmake-install
 # add both the top-level directory and the bin directory from the archive
 # to the system PATH. By adding it to the front of the path we hide the
