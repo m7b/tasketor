7a2df2fafc813342bd7a74f1efad1cca9d42a0ea
Mark Buttig <mbuttig@gmx.net>
2021-02-21 14:26:44 +0100

    alternative to travis_retry()

--- scripts/ci/update_cmake.sh
+++ scripts/ci/update_cmake.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-# fail script immediately on any errors in external commands
-set -e
 
-source scripts/ci/travis_retry.sh
+#https://github.com/travis-ci/travis-build.git/lib/travis/build/script/templates/header.sh
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    "$@"
+    result=$?
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+
+  [ $count -gt 3 ] && {
+    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+
+  return $result
+}
 
 
 # first we create a directory for the CMake binaries
