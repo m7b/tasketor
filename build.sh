665b704bcbd7b49e08d42d8ff7852f062c516883
Mark Buttig <mbuttig@gmx.net>
2021-02-25 14:12:02 +0100

    Boost build commands changed

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #install libs (boost, mariadb) and cmake
 #sudo apt-get -yq --no-install-suggests --no-install-recommends install libboost-all-dev libmariadb-dev cmake
 
-rm -r build
+rm -r -f build
 mkdir build
 cd build
 cmake ..
