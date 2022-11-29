9777900c670433cb25230466478a06a164943bf1
Mark Buttig <mbuttig@gmx.net>
2022-11-29 21:25:59 +0100

    new compiler etc.

--- scripts/__environment.bat
+++ scripts/__environment.bat
@@ -7,4 +7,5 @@ SET PATH_TO_TOOLS=C:\CPP\tools
 SET PATH_TO_EXT_LIBS=C:\CPP\extlibs
 REM SET PATH_TO_COMPILLER=C:\CPP\compiler\i686-8.1.0-release-posix-dwarf-rt_v6-rev0\mingw32
 REM SET PATH_TO_COMPILLER=C:\CPP\compiler\TDM-GCC-64
-SET PATH_TO_COMPILLER=C:\CPP\compiler\winlibs-x86_64-posix-seh-gcc-12.1.0-mingw-w64ucrt-10.0.0-r2\mingw64
+REM SET PATH_TO_COMPILLER=C:\CPP\compiler\winlibs-x86_64-posix-seh-gcc-12.1.0-mingw-w64ucrt-10.0.0-r2\mingw64
+SET PATH_TO_COMPILLER=C:\CPP\compiler\winlibs-x86_64-posix-seh-gcc-12.2.0-mingw-w64ucrt-10.0.0-r2\mingw64
